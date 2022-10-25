f76302ef40668b6e70c744c9ed1c71e8b94d0bff
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2022-10-25 10:23:31 +0200

    First commit

---
+++ deployment/cloudscale-lpg-2/trigger.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+watch "curl --silent --header \"Accept: application/json\" https://vpa-example-test.apps.cloudscale-lpg-2.appuio.cloud/"
---
+++ deployment/exoscale-ch-gva-2-0/trigger.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+watch "curl --silent --header \"Accept: application/json\" https://vpa-example-test.apps.exoscale-ch-gva-2-0.appuio.cloud/"
